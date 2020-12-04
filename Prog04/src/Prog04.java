83ec3c74fcd935af9ac7855c83b9160e02939bc3
do1do1 <do1@email.arizona.edu>
2020-12-04 11:56:32 -0700

    First commit

---
+++ Prog04/src/Prog04.java
@@ -0,0 +1,33 @@
+import java.sql.Statement;
+
+public class Prog04 {
+	public static void main(String[] args) {
+		
+	}
+	
+	class Employee{
+		public Employee() {
+			
+		}
+		public void getEmployee(Statement stmt) {
+			
+		}
+	}
+	class Member{
+		
+	}
+	class Product{
+		
+	}
+	class SalesRecord{
+		
+	}
+	class SubRecord{
+		
+	}
+	class Supplier{
+		
+	}
+
+}
+
